c29aa205b355ec2bfe1c3a6ac815d37ab2ec7613
Sascha Peter Bajonczak <Sascha.Bajonczak@realcore.de>
2025-06-15 17:30:15 +0200

    added shopify temaplte

---
+++ openscad/tag_inserts.sql
@@ -0,0 +1,147 @@
+INSERT INTO TravelTag (
+    tagId,
+    hasData,
+
+    ownerFirstName,
+    ownerLastName,
+    ownerAddress,
+    ownerEmail,
+    ownerMobile,
+    ownerLandline,
+    ownerOther,
+
+    guideFirstName,
+    guideLastName,
+    guideEmail,
+    guideMobile,
+    guideLandline,
+
+    destinationAccommodation,
+    destinationAddress,
+    transportation,
+    transportationNumber,
+    transportationDate
+) VALUES (
+    'fb981362-8f85-480e-8e12-b2162ffd9161',
+    0,
+
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    '',
+    '',
+    '',
+    '',
+    ''
+);
+
+
+INSERT INTO TravelTag (
+    tagId,
+    hasData,
+
+    ownerFirstName,
+    ownerLastName,
+    ownerAddress,
+    ownerEmail,
+    ownerMobile,
+    ownerLandline,
+    ownerOther,
+
+    guideFirstName,
+    guideLastName,
+    guideEmail,
+    guideMobile,
+    guideLandline,
+
+    destinationAccommodation,
+    destinationAddress,
+    transportation,
+    transportationNumber,
+    transportationDate
+) VALUES (
+    '0fe26b12-8992-4bd5-967c-d13fb256c9fc',
+    0,
+
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    '',
+    '',
+    '',
+    '',
+    ''
+);
+
+
+INSERT INTO TravelTag (
+    tagId,
+    hasData,
+
+    ownerFirstName,
+    ownerLastName,
+    ownerAddress,
+    ownerEmail,
+    ownerMobile,
+    ownerLandline,
+    ownerOther,
+
+    guideFirstName,
+    guideLastName,
+    guideEmail,
+    guideMobile,
+    guideLandline,
+
+    destinationAccommodation,
+    destinationAddress,
+    transportation,
+    transportationNumber,
+    transportationDate
+) VALUES (
+    'fed788bb-a558-4f8c-8e74-fdf434bc76c4',
+    0,
+
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    '',
+    '',
+    '',
+    '',
+    '',
+
+    '',
+    '',
+    '',
+    '',
+    ''
+);
+
+
